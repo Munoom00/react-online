b7e6d01d7d2e5d7437071d50f6ee7ae2bae276ec
Munoom00 <ta.tanva_st@tni.ac.th>
2021-11-27 13:28:23 +0700

    frist commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+
+    const nickName = "Mon"
+
+    return (
+        <div>
+            <h1>Tanva Taemthong {nickName}</h1>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+    
+    let companyName = "TNI"
+    // const companyAddress = <p>Bangkok</p>
+    // let number = 10
+
+    const showMessage = () => {
+        return companyName + ".com"
+    }
+
+    return (
+        <div>
+            <h1>Hello {showMessage()}</h1>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header
