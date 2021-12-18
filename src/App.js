2be1a84ba3526dd557b6240570b60bb79499ec96
Munoom00 <ta.tanva_st@tni.ac.th>
2021-12-18 09:42:36 +0700

    React-Router

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import Sidebar from "./components/Sidebar";
 import Menu from "./components/Menu";
 import HomePage from "./components/pages/HomePage";
 import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AboutPage from "./components/pages/AboutPage";
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <HomePage />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
